7f549596c28ab64b9b20ce1871c449fd12d1832f
macbook <macbook@macbookui-MacBookPro.local>
2021-01-24 10:33:15 +0900

    Fix some code

--- buildLFT.sh
+++ buildLFT.sh
@@ -8,6 +8,12 @@ cd ..
 cd gifmaker
 npm run build
 cd ..
+cd movie_app
+npm run build
+cd ..
+cd nomflix
+npm run build
+cd ..
 cd ..
 cd javascriptStudy
 cd cityroads
@@ -17,5 +23,7 @@ cd ..
 cd htmlCssStudy
 cp -rf "/Users/macbook/Projects/reactStudy/littlefunthings/build/." "/Users/macbook/Projects/htmlCssStudy/littlefunthings"
 cp -rf "/Users/macbook/Projects/reactStudy/gifmaker/build/." "/Users/macbook/Projects/htmlCssStudy/littlefunthings/gifmaker"
+cp -rf "/Users/macbook/Projects/reactStudy/nomflix/build/." "/Users/macbook/Projects/htmlCssStudy/littlefunthings/nomflix"
+cp -rf "/Users/macbook/Projects/reactStudy/movie_app/build/." "/Users/macbook/Projects/htmlCssStudy/littlefunthings/movie"
 cp -rf "/Users/macbook/Projects/javascriptStudy/cityroads/dist/." "/Users/macbook/Projects/htmlCssStudy/littlefunthings/cityroads"
 cp -rf "/Users/macbook/Projects/htmlCssStudy/animalFace/." "/Users/macbook/Projects/htmlCssStudy/littlefunthings/animalface"
---
+++ littlefunthings/movie/precache-manifest.b55295b3547b2a48daef90fec8225e2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63546ff0280328cb7adb36059833a209",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4d84c65d245b5ed235cd",
+    "url": "./static/css/main.672a5149.chunk.css"
+  },
+  {
+    "revision": "3292616cc5c511783277",
+    "url": "./static/js/2.558ee272.chunk.js"
+  },
+  {
+    "revision": "4d84c65d245b5ed235cd",
+    "url": "./static/js/main.d945746b.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
