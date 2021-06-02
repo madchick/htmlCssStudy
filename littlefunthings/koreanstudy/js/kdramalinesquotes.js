875a5fe04cf422eee126e30ddb14124fbdc812b8
madchick <madchick@gmail.com>
2021-06-02 10:02:51 +0900

    Fix pass parameter features

--- littlefunthings/koreanstudy/js/kdramalinesquotes.js
+++ littlefunthings/koreanstudy/js/kdramalinesquotes.js
@@ -63,7 +63,26 @@ let linesIndexMax;
 let linesData;
 
 function onload() {
-    loadQuotesData("0008")
+    var Request = function() {  
+        this.getParameter = function(name) {  
+            var rtnval = '';  
+            var nowAddress = unescape(location.href);  
+            var parameters = (nowAddress.slice(nowAddress.indexOf('?') + 1,  
+                    nowAddress.length)).split('&');  
+            for (var i = 0; i < parameters.length; i++) {  
+                var varName = parameters[i].split('=')[0];  
+                if (varName.toUpperCase() == name.toUpperCase()) {  
+                    rtnval = parameters[i].split('=')[1];  
+                    break;  
+                }  
+            }  
+            return rtnval;  
+        }  
+    }  
+    var request = new Request();
+    var paramValue = request.getParameter('id');  
+
+    loadQuotesData(paramValue)
         .then(
             (result) => { 
                 // console.log(result);
