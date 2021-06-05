18e846faabb3ab720b064d67349e2f741746dc20
madchickk <madchick@gmail.com>
2021-06-05 18:38:08 +0900

    roll back changes

---
+++ koreanstudy/js/index.js
@@ -0,0 +1,46 @@
+function loadXHR() {
+    return new Promise((resolve, reject) => {
+        try {
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", "./data/index.json");
+            xhr.onerror = event => {
+                reject(`Network error: ${event}`);
+            };
+            xhr.onload = () => {
+                if (xhr.status === 200) {
+                    resolve(xhr.response);
+                } else {
+                    reject(`XHR load error: ${xhr.statusText}`);
+                }
+            };
+            xhr.send();
+        } catch (err) {
+            reject(err.message);
+        }
+    });
+}
+
+function onload() {
+    loadXHR()
+        .then(
+            (result) => { 
+                // console.log(result);
+                renderDramaList(result);
+            },
+            (error) => { 
+                console.log(error);
+                return "";
+            }           
+        );
+}
+
+function renderDramaList(jsonData) {
+    if (jsonData!="")
+    {
+        dramaList = JSON.parse(jsonData);
+        newHTMLData = "";
+        for(i=0 ; i<dramaList.length ; i++)
+            newHTMLData += "<div class='item-" + (i+1) + "'><a href='kdramalinesquotes.html?id=" + dramaList[i].dramaid + "' class='card'><div class='thumb' style='background-image: url(" + dramaList[i].image + ");'></div><article><h1>" + dramaList[i].title + "</h1><p>" + dramaList[i].desc + "</p><span>" + dramaList[i].desc2 + "</span></article></a></div>";
+        document.getElementById('kdramalist').innerHTML = newHTMLData;
+    }
+}
